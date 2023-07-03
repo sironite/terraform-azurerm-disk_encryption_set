db69e8210c0471be39384ebb1ffc8ce0bcd7bcde
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-07-03 19:09:28 +0200

    feat: first module release

--- docs/header-doc.tf
+++ docs/header-doc.tf
@@ -1,6 +1,7 @@
 /**
- *  # Invullen
- * [![Changelog](https://img.shields.io/badge/changelog-release-green.svg)](Invullen) [![Notice](https://img.shields.io/badge/notice-copyright-yellow.svg)](NOTICE) [![Apache V2 License](https://img.shields.io/badge/license-Apache%20V2-orange.svg)](LICENSE) [![TF Registry](https://img.shields.io/badge/terraform-registry-blue.svg)](Invullen)
+ *  ## Disk Encryption Set
+ *
+ * [![Changelog](https://img.shields.io/badge/changelog-release-green.svg)](https://github.com/sironite/terraform-azurerm-disk_encryption_set/releases/latest) [![Notice](https://img.shields.io/badge/notice-copyright-yellow.svg)](NOTICE) [![Apache V2 License](https://img.shields.io/badge/license-Apache%20V2-orange.svg)](LICENSE) [![TF Registry](https://img.shields.io/badge/terraform-registry-blue.svg)](https://registry.terraform.io/providers/hashicorp/azurerm/3.63.0/docs/resources/disk_encryption_set)
  *
  * 
  *
---
+++ examples/DES/main.tf
@@ -0,0 +1,17 @@
+
+
+module "disk_encryption_set" {
+  source  = "sironite/disk_encryption_set/azurerm"
+version = "X.X.X"
+
+  name                      = "my-disk-encryption-set"
+  resource_group_name       = "my-resource-group"
+  location                  = "westeurope"
+  key_vault_key_id          = "/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/my-resource-group/providers/Microsoft.KeyVault/vaults/my-key-vault/keys/my-key"
+  auto_key_rotation_enabled = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
